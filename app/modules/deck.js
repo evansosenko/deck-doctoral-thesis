e16a421f6fddb1903f9cc39f5d500ca879153788
Evan Sosenko <razorx@evansosenko.com>
2016-06-22 15:18:25 -0700

    Add bespoke plugins: classes, hash, touch

--- app/modules/deck.js
+++ app/modules/deck.js
@@ -1,8 +1,14 @@
 import bespoke from 'bespoke'
+import classes from 'bespoke-classes'
+import hash from 'bespoke-hash'
 import keys from 'bespoke-keys'
+import touch from 'bespoke-touch'
 
 const plugins = [
-  keys()
+  classes(),
+  hash(),
+  keys(),
+  touch()
 ]
 
 export default (element) => {
