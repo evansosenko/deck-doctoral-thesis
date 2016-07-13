14d694307f0646a6e9727abe0f8d59c7e6bd2d41
Evan Sosenko <razorx@evansosenko.com>
2016-07-12 18:50:59 -0700

    Update webfontloader to 1.6.26

--- vendor/fonts.js
+++ vendor/fonts.js
@@ -13,6 +13,6 @@ window.WebFontConfig = {
   var el = d.createElement('script')
   var s = d.scripts[0]
   el.async = 1
-  el.src = 'https://cdnjs.cloudflare.com/ajax/libs/webfont/1.6.24/webfontloader.js'
+  el.src = 'https://cdnjs.cloudflare.com/ajax/libs/webfont/1.6.26/webfontloader.js'
   s.parentNode.insertBefore(el, s)
 })(document)
